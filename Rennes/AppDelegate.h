d5a95c340442775370cb6264409f9f8cbcf109dd
Adrien Humili√®re <hello@adhumi.fr>
2015-09-28 15:03:30 +0200

    Initial Commit

---
+++ Rennes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rennes
+//
+//  Created by Adrien Humilière on 28/09/2015.
+//  Copyright © 2015 adhumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rennes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rennes
+//
+//  Created by Adrien Humilière on 28/09/2015.
+//  Copyright © 2015 adhumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
